9e2e3a3ed39f993dc661a90831f1d0f49d9b6390
Marco Arnulfo <arnulfomarco7@gmail.com>
2023-01-30 18:54:24 +0100

    add btn red

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -6,6 +6,7 @@
 <template>
     <h2 class="my-5">finito</h2>
     <button class=" p-4 btn btn-primary">btn bello</button>
+    <button class="btn btn-danger">btn rosso </button>
 </template>
 
 <style lang="scss">
