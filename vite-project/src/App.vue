dad36a4fa44398d5883647a8dde4d69cdd7f976b
federossi93 <rossifederico0993@gmail.com>
2023-01-30 18:33:44 +0100

    complete the requested cheanges
    add bootstrap

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -4,9 +4,11 @@
 </script>
 
 <template>
-    <h2>finito</h2>
+    <h2 class="my-5">finito</h2>
+    <button class=" p-4 btn btn-primary">btn bello</button>
 </template>
 
 <style lang="scss">
+@use './styles/general.scss';
 
 </style>
